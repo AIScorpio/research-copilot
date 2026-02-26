001b35952ddd4636cd480e2181100108c674e8ea
AIScorpio <leon.x.chao@gmail.com>
2026-02-26 19:01:43 +0800

    Reset migrations for PostgreSQL - use db push instead of migrate deploy

--- prisma/migrations/20260213053031_add_relevance_scores/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Paper" ADD COLUMN "assessmentReason" TEXT;
-ALTER TABLE "Paper" ADD COLUMN "businessScore" REAL;
-ALTER TABLE "Paper" ADD COLUMN "practicalityScore" REAL;
-ALTER TABLE "Paper" ADD COLUMN "relevanceScore" REAL;
-ALTER TABLE "Paper" ADD COLUMN "technicalScore" REAL;
-ALTER TABLE "Paper" ADD COLUMN "timelinessScore" REAL;
